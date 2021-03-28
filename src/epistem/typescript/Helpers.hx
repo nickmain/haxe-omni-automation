e40bb3a3e8e67d0bb8f469339e15c492262b9163
Nick Main <david.nick.main@gmail.com>
2021-03-27 17:33:53 -0700

    add union helpers for 5 and 6

--- src/epistem/typescript/Helpers.hx
+++ src/epistem/typescript/Helpers.hx
@@ -6,3 +6,5 @@ package epistem.typescript;
 abstract Union2<A,B>(Dynamic) from A to A from B to B {}
 abstract Union3<A,B,C>(Dynamic) from A to A from B to B from C to C {}
 abstract Union4<A,B,C,D>(Dynamic) from A to A from B to B from C to C from D to D {}
+abstract Union5<A,B,C,D,E>(Dynamic) from A to A from B to B from C to C from D to D from E to E {}
+abstract Union6<A,B,C,D,E,F>(Dynamic) from A to A from B to B from C to C from D to D from E to E from F to F {}
